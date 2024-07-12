002128e196dbfb2d27be7f4e3ab57d33c0ff4688
GUNGODD <chauhanadityac01@gmail.com>
2024-07-12 10:08:23 +0000

    header

---
+++ app/header.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/Mode-toggle";
+import { api } from "@/convex/_generated/api";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
+import Image from "next/image";
+export default function Header() {
+  const documents = useQuery(api.documents.getDocuments);
+  const createDocument = useMutation(api.documents.creteDocument);
+
+  return (
+    <div className="bg-slate-900 py-4 ">
+      <div className="container mx-auto flex justify-between items-center ">
+        <div className="flex items-center gap-4">
+          <Image src="/logo.svg" width={40} height={40} alt="Ameleia Logo " />
+          AMELIEA
+        </div>
+
+        <div>
+          <Unauthenticated>
+            <SignInButton></SignInButton>
+          </Unauthenticated>
+
+          <Authenticated>
+            <div className="flex gap-4">
+              <ModeToggle />
+              <UserButton />
+            </div>
+          </Authenticated>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// mx-auto  container everything in container class will conatain  full of screen
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/ui/Mode-toggle";
-import { api } from "@/convex/_generated/api";
-import { SignInButton, UserButton } from "@clerk/nextjs";
-import {
-  Authenticated,
-  Unauthenticated,
-  useMutation,
-  useQuery,
-} from "convex/react";
+import Header from "./header";
 
 export default function Home() {
   {
@@ -22,34 +13,9 @@ export default function Home() {
     /*TODO: getDocument-> it is used to create a table inside   convex db an then insert data to  following attributes eg title  name , etc etc */
   }
 
-  const documents = useQuery(api.documents.getDocuments);
-  const createDocument = useMutation(api.documents.creteDocument);
-
   return (
-    <main>
-      <h1> hello world </h1>
-
-      <Unauthenticated>
-        <SignInButton></SignInButton>
-      </Unauthenticated>
-
-      <Authenticated>
-        <UserButton />
-
-        <ModeToggle />
-
-        <Button
-          onClick={() => {
-            createDocument({ title: "hello Convex  i am aditya" });
-          }}
-        >
-          Click me{" "}
-        </Button>
-
-        {documents?.map((doc) => {
-          return <div key={doc._id}> {doc.title}</div>;
-        })}
-      </Authenticated>
-    </main>
+    <>
+      <Header />
+    </>
   );
 }
